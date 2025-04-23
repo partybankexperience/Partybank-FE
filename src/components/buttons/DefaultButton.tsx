29e612996acad45aabc84f211cbf2b18c204b114
ololade <akin0098@algonquinlive.com>
2025-04-23 11:34:01 -0400

    feat: update DefaultInput with validation, icons, dropdown, and password toggle

---
+++ src/components/buttons/DefaultButton.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+type ButtonProps = {
+  variant?: 'primary' | 'secondary' | 'black'| 'tertiary' ;
+  type?: 'default' | 'icon-left' | 'icon-right';
+  size?: "normal" | "small";
+  icon?: React.ReactNode;
+  children: React.ReactNode;
+  disabled?: boolean;
+  onClick?: () => void;
+};
+
+const DefaultButton = ({
+  variant = "primary",
+  size = "normal",
+  icon,
+  children,
+  disabled = false,
+  onClick,
+  type = "default",
+}: ButtonProps) => {
+    const variantClasses = {
+        primary: 'bg-primary text-white hover:bg-pink focus:border-darkRed disabled:bg-mutedBlueGrey disabled:!text-white',
+        secondary: 'border-[1px] text-black border-primary hover:text-primary focus:text-darkRed disabled:border-mutedBlueGrey focus:border-darkGrey',
+        tertiary: ' hover:text-darkRed focus:text-darkRed text-primary disabled:border-mutedBlueGrey  focus:border-[1px] focus:border-darkGrey  disabled:!bg-none',
+        black: 'bg-black text-white hover:bg-grey focus:bg-grey focus:border-[1px] focus:border-black disabled:bg-mutedBlueGrey disabled:!text-white',
+      };
+  const baseStyle = " rounded-[32px] text-[20px] w-fit cursor-pointer disabled:cursor-not-allowed disabled:border-mutedBlueGrey disabled:text-mutedBlueGrey";
+
+  const sizeStyle = size === "small" ? "py-[12px] px-[24px] text-[16px]" : "py-[10px] px-[32px]";
+
+  const iconLeft = type === "icon-left" && icon ? (
+    <span className="mr-2">{icon}</span>
+  ) : null;
+
+  const iconRight = type === "icon-right" && icon ? (
+    <span className="ml-2">{icon}</span>
+  ) : null;
+
+  return (
+    <button className={`${baseStyle} ${sizeStyle} ${variantClasses[variant]}`} disabled={disabled} onClick={onClick}>
+      <div className="flex items-center justify-center">
+        {iconLeft}
+        {children}
+        {iconRight}
+      </div>
+    </button>
+  );
+};
+
+export default DefaultButton;
+
+// import { FaCheckCircle } from "react-icons/fa";
+
+// const SampleUsage = () => {
+//     return (
+//         <div>
+//             <DefaultButton
+//                 variant="primary"
+//                 type="icon-left"
+//                 size="small"
+//                 icon={<FaCheckCircle />}
+//                 disabled={false}
+//                 onClick={() => console.log("Button clicked!")}
+//             >
+//                 Confirm
+//             </DefaultButton>
+//         </div>
+//     );
+// };
+
+// export default SampleUsage;
